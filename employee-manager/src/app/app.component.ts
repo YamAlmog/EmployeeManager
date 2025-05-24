1a6ea193bec316631279cb8c67a76eb751274e7e
YamAlmog <yamalmog14l@gmail.com>
2025-05-25 01:57:31 +0300

    akitaDevtools

--- employee-manager/src/app/app.component.ts
+++ employee-manager/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { akitaDevtools } from '@datorama/akita';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    if (isDevMode()) {
+      akitaDevtools();
+    }
+  }
+}
